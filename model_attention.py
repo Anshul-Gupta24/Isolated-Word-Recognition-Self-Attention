9c39a5c58799cbf59867509652366b4c7a51ba94
Anshul Gupta <anshul624.gupta@gmail.com>
2019-02-21 12:03:48 +0530

    Add files via upload

---
+++ model_attention.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import keras.backend as K
+from keras.layers import Input, Lambda, LSTM, Activation, concatenate, Dropout, warnings, Flatten, Dense, TimeDistributed, Multiply, Masking, Embedding
+from keras.models import Model, Sequential
+from keras.engine.topology import get_source_inputs
+from keras.utils import get_file, plot_model, layer_utils
+from keras.activations import softmax
+
+
+class WordNet():
+	def __init__(self):
+		self.model = None
+		self.model = self._audio_submodel()
+		self.model.summary()
+        
+
+	def __call__(self, model_option):
+		if model_option == "train":
+        		return self.model
+		else:
+        		return None
+    
+
+	def _audio_submodel(self):
+        
+		hidden_size = 128
+		input_size = 80		# change depending on pretrained dnn
+		output_classes = 662
+
+		inp = Input((None, input_size))
+		inp2 = Masking(mask_value=0.0)(inp)
+		lstm = LSTM(hidden_size, return_sequences=False)(inp2)
+
+		# Attention Part (uncomment)
+
+		#lstm = LSTM(hidden_size, return_sequences=True)(lstm)
+		#attention = TimeDistributed(Dense(1))(lstm)		# attention weights!!
+		#attention = Lambda(lambda x: softmax(x, axis=1))(attention)
+		#context = Multiply()([attention,lstm])
+		#out = Lambda(lambda x: K.sum(x,axis=1))(context)		
+		out = Dense(4096, activation='relu')(lstm)
+		out = Dense(output_classes, activation='softmax')(out)
+		model = Model(inputs=inp,outputs=[out])			
+		
+        
+		return model
+    
+
---
+++ train_dataset.py
@@ -0,0 +1,138 @@
+from model_attention import WordNet
+import keras	
+import numpy as np
+from keras.utils import plot_model
+import os
+import pickle
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import functions
+from keras.callbacks import ModelCheckpoint
+import sklearn.model_selection
+import timeit
+import json
+
+
+if __name__ == '__main__':
+
+    start = timeit.default_timer()
+    BATCH_SIZE = 32
+    FEATURE_SIZE = 80   
+
+
+    model = WordNet()("train")
+    model.compile(optimizer=keras.optimizers.Adam(lr=1e-4, decay=1e-5), 
+                loss='categorical_crossentropy',
+                metrics=['accuracy'])
+
+
+    # Get Data
+    with open('/home/data1/anshulg/google_synth_features2', 'rb') as fp:
+        data = pickle.load(fp) 
+
+    lst = list(data.keys())
+    labels = [l[17:functions.get_sec_und(l)-1] for l in lst]
+    classes = set(labels)
+    class_enc = {}
+    for i,c in enumerate(classes):
+        class_enc[c] = i
+
+
+    label_vecs = np.zeros((len(lst), len(classes)))
+    for i,lv in enumerate(label_vecs):
+       lv[class_enc[labels[i]]] = 1
+
+    lst_train, lst_val, label_vecs_train, label_vecs_val = sklearn.model_selection.train_test_split(lst, label_vecs, test_size=0.2)
+    print(len(lst))
+    print(len(lst_train))
+    print(len(lst_val))
+
+    # with open('/home/anshulg/WordNet/wordnet_val.pkl', 'rb') as fp:
+    #     lst_val = pickle.load(fp)
+
+    # lst_train = []
+    # labels_train = []
+    # labels_val = []
+    # for i,l in enumerate(lst):
+    #     if l not in lst_val:
+    #         lst_train.append(l)
+    #         labels_train.append(labels[i])
+        
+    # labels_val = [l[17:functions.get_sec_und(l)-1] for l in lst_val]
+
+    # label_vecs_train = np.zeros((len(lst_train), len(classes)))
+    # for i,lv in enumerate(label_vecs_train):
+    #     lv[class_enc[labels_train[i]]] = 1
+
+    # label_vecs_val = np.zeros((len(lst_val), len(classes)))
+    # for i,lv in enumerate(label_vecs_val):
+    #     lv[class_enc[labels_val[i]]] = 1
+
+    # lst_train, label_vecs_train = sklearn.utils.shuffle(lst_train, label_vecs_train)
+
+
+    def generator(lst, label_vecs):
+        batch_size = BATCH_SIZE
+        features = FEATURE_SIZE
+        i = 0
+        while(True):
+
+            j=i+batch_size
+            if j>=len(lst):
+                i = 0
+                j=i+batch_size
+
+
+            seq_lens = [len(data[lst[x]]) for x in range(i,j)]
+            max_len = max(seq_lens)			# Get max sequence length
+
+            batch_data = np.zeros((batch_size, max_len, features))
+            batch_labels = np.array(label_vecs[i:j])
+
+            c=0
+            for x in range(i,j):
+                batch_data[c, :seq_lens[c], :]=data[lst[x]]
+                c+=1
+
+            i=j
+                        
+            yield batch_data, batch_labels
+
+        
+
+    filepath = "/home/data1/anshulg/model_new_dataset3.h5"
+    checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, mode='min', period=1)
+    callbacks_list = [checkpoint]
+
+
+    train_size = len(lst_train)
+    val_size = len(lst_val)
+
+
+    history = model.fit_generator(generator(lst_train, label_vecs_train), steps_per_epoch=int(train_size/BATCH_SIZE), epochs=80, callbacks=callbacks_list, validation_data=generator(lst_val, label_vecs_val), validation_steps=int(val_size/BATCH_SIZE))
+
+
+    print(history.history.keys())
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.savefig("model_loss_newdata3.png", bbox_inches = 'tight', pad_inches = 0)
+    plt.close()
+
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.ylabel('acc')
+    plt.xlabel('epoch')
+    plt.savefig("model_acc_newdata3.png", bbox_inches = 'tight', pad_inches = 0)
+    plt.close()
+
+    with open('/home/data1/anshulg/history_newdata3.json', 'w') as fp:
+        json.dump(history.history, fp)
+
+    stop = timeit.default_timer()
+    time = open('time.txt','w')
+    time.write(str(stop-start))
